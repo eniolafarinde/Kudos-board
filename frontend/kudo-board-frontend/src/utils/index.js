97d18215aaf85801240b01b5e62fbaf8d3d7c7b9
Eniola Olawumi Farinde <eniolafarinde@gmail.com>
2025-06-20 15:20:08 -0700

    troubleshooting

--- frontend/kudo-board-frontend/src/utils/index.js
+++ frontend/kudo-board-frontend/src/utils/index.js
@@ -1,5 +1,9 @@
 export function getBaseUrl() {
+    return "https://kudos-board-fpm6.onrender.com"
+    
     return (
+
         import.meta.env.VITE_DEV ? "http://localhost:3000" : "https://kudos-board-fpm6.onrender.com"
+
     )
 }
